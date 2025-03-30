806a42b523411bbdc92377c149720ddc373d9ccd
hanifd3v <168025373+hanifd3v@users.noreply.github.com>
2025-03-30 11:03:24 +0700

    Update DevClass.java rollback

--- src/main/java/com/hanifiamdev/DevClass.java
+++ src/main/java/com/hanifiamdev/DevClass.java
@@ -8,7 +8,6 @@ public class DevClass
 {
     public static void main( String[] args )
     {
-        System.out.println( "DevClass+another-user" );
-        // Checkin from another user
+        System.out.println( "DevClass" );
     }
 }
